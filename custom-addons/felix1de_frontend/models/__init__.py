6f4e26214a0a7a5318b04c0918a1c1850621a2e3
Unknown <hen@hiyacoding.co.uk>
2016-12-10 09:15:11 -0800

    struktured views

--- custom-addons/felix1de_frontend/models/__init__.py
+++ custom-addons/felix1de_frontend/models/__init__.py
@@ -3,10 +3,8 @@ import res_partner
 import branch
 import order
 import customer_mail_send
-#import customer
 import product
 import mandanten
-import kontakte
 import kanzleien
 import project
 import partner
---
+++ custom-addons/felix1de_frontend/views/own_menus/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from . import backend.xml
+from . import start
