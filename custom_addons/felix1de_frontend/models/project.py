2e9f72f6d2c1501cd1c32e93717575f75989a8c6
skshivammahajan <skshivammahajan@gmail.com>
2016-12-17 02:26:29 +0530

    Workflow Template

--- custom_addons/felix1de_frontend/models/project.py
+++ custom_addons/felix1de_frontend/models/project.py
@@ -15,6 +15,20 @@ class ProjectIssue(models.Model):
     tikStartDate=fields.Date('Erstellt Am')
     tikCloseDate=fields.Date('Geschlossen Am')
     ticket_id=fields.Many2one('felix1.ticket', string="Ticket")
+    priority = fields.Selection([('0','Low'), ('1','Normal'),('2','Intermediate'),('3','High')], 'Priority', select=True)
+    state=fields.Selection([('new', 'New'),('progress','Progress'),('done', 'Done'),('cancel', 'Cancel')], string='Status',  default='new')
+    # state = fields.Many2one('project.task.type',string='Stage State')
+    # state = fields.Selection(related='stage_id.id',string='Stage State')
+
+    @api.multi
+    def progress_value(self):
+        self.write({'state':'progress'})
+    @api.multi
+    def cancel_value(self):
+        self.write({'state':'cancel'})
+    @api.multi
+    def done_value(self):
+        self.write({'state':'done'})
 
     
 class ProjectTask(models.Model):
@@ -35,3 +49,5 @@ class BackendLine(models.Model):
     einmalgebuhr=fields.Float('Einmalgebuhr')
     jahresgebuhr=fields.Float('Jahresgebuhr')
     #order_id=fields.Many2one('client.order')
+
+
