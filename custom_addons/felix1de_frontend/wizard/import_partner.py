e7018323e1ecdad25265381b63e7f0183de47882
vipin <vipintripathi310@gmail.com>
2017-01-19 02:15:54 +0530

    Mapping of the mandant and kontakt data

--- custom_addons/felix1de_frontend/wizard/import_partner.py
+++ custom_addons/felix1de_frontend/wizard/import_partner.py
@@ -9,16 +9,17 @@ import csv
 import base64
 import StringIO
 import cStringIO 
+from xlrd import open_workbook
 
 class res_partner_import(models.Model):
     _name='res.partner.import'
     partner_type = fields.Selection([('mandantain','Mandanten'),
-                                     ('kontakte','Kontakte')],'Partner Data Type')
+                                     ('kontakte','Kontakte'),
+                                     ('mapping_data','Mapping Data')],'Partner Data Type')
     data = fields.Binary('Data')
     
-    
-     
-    def import_partner_data(self, cr, uid, ids, context=None):
+    def import_excel_sheet(self, cr, uid, ids, context=None):
+        partner_pool = self.pool.get('res.partner')
         file_name = self.browse(cr, uid, ids)[0]
         if not file_name.data:
             raise osv.except_osv(_('File Not Chosen!'), _('Please Choose The File!'))
@@ -26,54 +27,85 @@ class res_partner_import(models.Model):
         if not str_data:
             raise osv.except_osv('Warning', 'No File Data')
         try:
-            partner_data = csv.DictReader(cStringIO.StringIO(str_data), dialect="excel")
+            partner_data = csv.DictReader(cStringIO.StringIO(str_data), delimiter=',', quotechar='"')
+        except:
+            raise osv.except_osv('Warning', 'Make sure you saved the file as .csv extension and import!')
+        count=0
+        for line in partner_data:
+            count+=1
+            kontakt_id = partner_pool.search(cr,uid,[('name','=',str(line.get('Nachname',''))),('ist_kontakt','=',True)],limit=1)
+            mandant_id = partner_pool.search(cr,uid,[('name','=',str(line.get('Mandant',''))),('ist_mandant','=',True)],limit=1)
+            print"kontakt_id",kontakt_id
+            print"mandant_id",mandant_id
+            print"count",count
+            if kontakt_id and mandant_id :
+                partner_pool.write(cr,uid,mandant_id,{
+                    'child_ids':[(6,0,kontakt_id)]
+                    })
+    
+     
+    def import_partner_data(self, cr, uid, ids, context=None):
+        partner_pool = self.pool.get('res.partner')
+        file_name = self.browse(cr, uid, ids)[0]
+        if not file_name.data:
+            raise osv.except_osv(_('File Not Chosen!'), _('Please Choose The File!'))
+        str_data = base64.decodestring(file_name.data)
+        print "str_data=========>",str_data
+        if not str_data:
+            raise osv.except_osv('Warning', 'No File Data')
+        try:
+            partner_data = csv.DictReader(cStringIO.StringIO(str_data), delimiter=',', quotechar='"')
             # saleorder_data = list(csv.reader(cStringIO.StringIO(str_data)))
         except:
             raise osv.except_osv('Warning', 'Make sure you saved the file as .csv extension and import!')
+        from pprint import pprint as pp
+        print"==================", pp(partner_data)
         for line in partner_data:
             dic={}
             print"line=======>",line
             from pprint import pprint as pp
             print pp(line)
-            if file_name.is_mandant :
+            if file_name.partner_type == 'mandantain' :
                 dic.update({
                     'ist_mandant':True,
-                    'is_kontakt':False,
+                    'ist_kontakt':False,
                     'street':line.get('Adresse',''),
                     'name':line.get('Mandant',''),
                     'BIC':line.get('BIC',''),
                     'email':line.get('eMailPISA','')
                     })
-            self.pool.get('res.partner').create(cr,uid,dic
-                )
-            if file_name.is_kontakt :
+            if file_name.partner_type == 'kontakte' :
                 dic.update({
-                    'is_kontakt':True,
+                    'ist_kontakt':True,
                     'ist_mandant':False,
                     'street':line.get('Adresse',''),
-                    'name':line.get('Mandant',''),
-                    'BIC':line.get('BIC',''),
-                    'email':line.get('eMailPISA','')
+                    'name':line.get('Nachname',''),
+                    'lastname':line.get('NachnameVorname',''),
+                    'phone':line.get('Telefon1',''),
+                    'email':line.get('eMail1',''),
+                    'company_type':'person'
                     })
-            partner_id = self.pool.get('res.partner').create(cr,uid,dic,context=None)
-            self.pool.get('bank.detail').create({
-                                                'iban':line.get('IBAN',''),
-                                                'name':line.get('Bank',''),
-                                                'bic':line.get('BIC',''),
-                                                'client_number':line.get('Mandantennummer','')
-                })
             
-            #count += 1
-            #emp_name = line.get('Name',False)
-            #other = [l.replace('/t', ',') for l in line]
-            #other1 = other[0].split()
-            #date = line.get('Date',False)
-            #status = line.get('Status',False)
-            #if not emp_name or not date:
-            #    raise osv.except_osv(
-            #            _('-'),
-            #            _('Any Column is blank in Row No %s ') % (count))
-
+            if file_name.partner_type in ('kontakte','mandantain'):
+                partner_id = self.pool.get('res.partner').create(cr,uid,dic,context=None)
+                print "partner_id===============>",partner_id
+                self.pool.get('bank.detail').create(cr,uid,{
+                                                    'iban':line.get('IBAN',''),
+                                                    'name':line.get('Bank',''),
+                                                    'bic':line.get('BIC',''),
+                                                    'client_number':line.get('Mandantennummer',''),
+                                                    'client_id':partner_id,
+                    })
+            if file_name.partner_type == 'mapping_data' :
+                kontakt_id = partner_pool.search(cr,uid,[('name','=',str(line.get('Nachname','')))],limit=1)
+                mandant_id = partner_pool.search(cr,uid,[('name','=',str(line.get('Vorname','')))],limit=1)
+                print"kontakt_id===>",kontakt_id
+                print"mandant_id======>",mandant_id
+                if kontakt_id and mandant_id :
+                    partner_pool.write(cr,uid,mandant_id,{
+                        'child_ids':[(6,0,kontakt_id)]
+                        })
+            return True
     
 res_partner_import()
     
